1b6d9af4f3a849e0ffe71e638850f2a23975325b
alejandro <alejandro.12w@gmail.com>
2022-08-11 15:06:13 -0700

    User Create, Strong user password, Get all users and especific user with password hiden

--- routers/users.js
+++ routers/users.js
@@ -1,34 +1,50 @@
 const { User } = require("../models/user");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
+//-----------------------------------------------READ ALL USERS---------------------
 router.get(`/`, async (req, res) => {
-  const userList = await User.find();
+  const userList = await User.find().select("-passwordHash");
 
   if (!userList) {
     res.status(500).json({ succes: false });
   }
   res.send(userList);
 });
-/*
-router.post(`/`, (req, res) => {
-  const product = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    countInStock: req.body.countInStock,
-  });
 
-  product
-    .save()
-    .then(createdProduct => {
-      res.status(201).json(createdProduct);
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err,
-        succes: false,
-      });
-    });
+//-----------------------------------------------READ ONE USER---------------------
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id).select("-passwordHash");
+
+  if (!user) {
+    res
+      .status(500)
+      .json({ message: "The user with the given ID is not found" });
+  }
+  res.status(200).send(user);
 });
-*/
+
+//-----------------------------------------------CREATE---------------------
+router.post("/", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    number: req.body.number,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+  user = await user.save();
+
+  if (!user) {
+    res.status(404).json("The user cannot be created");
+  }
+  res.send(user);
+});
+
 module.exports = router;
