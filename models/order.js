742662556ebf5c4fd2bda0de849c34380640562c
alejandro <alejandro.12w@gmail.com>
2022-08-18 14:43:44 -0700

    Models of Orders ok

--- models/order.js
+++ models/order.js
@@ -1,5 +1,62 @@
 const mongoose = require("mongoose");
 
-const orderSchema = mongoose.Schema({});
+const orderSchema = mongoose.Schema({
+  orderItems: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "OrderItems",
+      require: true,
+    },
+  ],
+  shippingAddress1: {
+    type: String,
+    requir: true,
+  },
+  shippingAddress2: {
+    type: String,
+    requir: true,
+  },
+  city: {
+    type: String,
+    requir: true,
+  },
+  zip: {
+    type: String,
+    requir: true,
+  },
+  country: {
+    type: String,
+    requir: true,
+  },
+  phone: {
+    type: String,
+    requir: true,
+  },
+  status: {
+    type: String,
+    requir: true,
+    default: "Pending",
+  },
+  totalPrice: {
+    type: Number,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    requir: true,
+  },
+  dateOrdered: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//Changing the "id" instead of "_id"
+orderSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+orderSchema.set("toJSON", {
+  virtuals: true,
+});
 
 exports.Order = mongoose.model("Order", orderSchema);
